b1cd8b403dcc1f5b0e98ddff5a17cc5aa5d10418
Maytham <44939173+Maytha8@users.noreply.github.com>
2022-08-21 16:50:21 +0800

    Modify header in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 /**
  * Externalise-Sourcemap
- * @file Externalise an inline sourcemap.
+ * Externalise an inline sourcemap.
  * Copyright (c) Maytham Alsudany 2022
- * @copyright Maytham Alsudany 2022
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
