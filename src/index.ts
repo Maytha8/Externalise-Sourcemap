9bb7624a7c1a3c292c6c99fc24b8f54e18ae3271
Maytham <44939173+Maytha8@users.noreply.github.com>
2022-08-25 19:56:17 +0800

    Update Sourcemap-matching Regex
    Inline source maps are base64 encoded data URIs. The regex has been updated to allow for an optional charset parameter in the data URI.

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,8 @@
  * along with this program. If not, see \<http://www.gnu.org/licenses/\>.
  */
 
-const SOURCEMAP_REGEX = /(?<=\/(\/|\*)[@#]\ssourceMappingURL=data:application\/json;base64,)[A-Za-z0-9=]*/m
-const SOURCEMAP_COMMENT_REGEX = /\/(\/|\*)[@#]\ssourceMappingURL=data:application\/json;base64,[A-Za-z0-9=]*/m
+const SOURCEMAP_REGEX = /(?<=\/(\/|\*)[@#]\ssourceMappingURL=data:application\/json(;charset\s*=[\s"']*([A-Za-z0-9-\s]*))?;base64,)[A-Za-z0-9=]*/m
+const SOURCEMAP_COMMENT_REGEX = /\/(\/|\*)[@#]\ssourceMappingURL=data:application\/json(;charset\s*=[\s"']*([A-Za-z0-9-\s]*))?;base64,[A-Za-z0-9=]*/m
 
 /**
  * Externalise a sourcemap from the provided input.
